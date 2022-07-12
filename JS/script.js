47a3100bbebf8c9274009d3d0eb9dbd3143624ad
Lizzle <leefboettcher@gmail.com>
2022-07-12 12:03:17 -0500

    favorites funtionality

--- JS/script.js
+++ JS/script.js
@@ -39,7 +39,7 @@ function getPokemonData() {
       var poke = JSON.stringify(data.results[19].name)
       var pokeEl = document.querySelector('#pokemon')
       // console.log(data.results[19].name)
-      var random = data.results[Math.floor(Math.random()*data.results.length)];
+      var random = data.results[Math.floor(Math.random() * data.results.length)];
       pokeEl.innerText = `Pokemon: ${random.name}`
       console.log(random.name)
 
@@ -60,9 +60,8 @@ function getExcuseData() {
       var excuse = data[3].excuse
       var excuseEl = document.querySelector('#excuse')
       excuseEl.innerText = `Excuse: ${excuse}`
-      // console.log(data[3].excuse)
     })
-
+    
 }
 
 generateBtn2.addEventListener("click", getExcuseData);
@@ -82,14 +81,54 @@ function getTriviaData() {
       console.log(data[19].question)
       console.log(data[19].correctAnswer)
     })
-
 }
 
 generateBtn3.addEventListener("click", getTriviaData);
 
+
+
+var favorites = [];
+  if (localStorage.getItem("favorites")) {
+    favorites = JSON.parse(localStorage.getItem("favorites"))
+  }
+
 var excuseFav = document.getElementById('fvr-excuse')
+var triviaFav = document.getElementById('fvr-trivia')
+var pokemonFav = document.getElementById('fvr-poke')
+var funFactFav = document.getElementById('fvr-fact')
+
 excuseFav.addEventListener("click", function () {
-  var favorites = document.getElementById('excuse').innerText;
-  console.log(favorites)
-  localStorage.setItem('favorites', JSON.stringify(favorites));
+  var excuseText = document.getElementById("excuse").innerHTML;
+  if (excuseText === "") {
+    return;
+  }
+  favorites.push(excuseText);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+});
+
+triviaFav.addEventListener("click", function () {
+  var triviaText = document.getElementById("question").innerHTML;
+  if (triviaText === "") {
+    return;
+  }
+  favorites.push(triviaText);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+});
+
+pokemonFav.addEventListener("click", function () {
+  var pokemonText = document.getElementById("pokemon").innerHTML;
+  if (pokemonText === "") {
+    return;
+  }
+  favorites.push(pokemonText);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+});
+
+funFactFav.addEventListener("click", function () {
+  var funFactText = document.getElementById("fact").innerHTML;
+  if (funFactText === "") {
+    return;
+  }
+  favorites.push(funFactText);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
 });
